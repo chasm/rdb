fb30a66c9fb4ae14e1132d0a4b8054b39a08de86
Charles F. Munat <charles.munat@gmail.com>
2015-06-09 21:35:46 +1200

    Basic app.

---
+++ index.js
@@ -0,0 +1,30 @@
+require("babel/register")
+
+const app       = require("koa")()
+const koaBody   = require("koa-better-body")
+const logger    = require("koa-logger")
+const koaMount  = require("koa-mount")
+const passport  = require("koa-passport")
+const KoaRouter = require("koa-router")
+const session   = require("koa-session")
+const koaStatic = require("koa-static")
+
+let router = new KoaRouter()
+
+app.use(koaBody())
+app.use(logger())
+
+app.keys = [ "1234567890" ]
+app.use(session(app))
+
+app.use(function *() {
+  var n = this.session.views || 0
+
+  this.session.views = ++n
+  this.body = n + " views"
+})
+
+app.listen(3000, function () {
+  console.log("Listening on port 3000.")
+})
+
