9d098ff273289106365573c8145a2ab2e2ac6444
Charles F. Munat <charles.munat@gmail.com>
2015-06-09 21:57:09 +1200

    RethinkDB working.

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ require("babel/register")
 
 const app       = require("koa")()
 const koaBody   = require("koa-better-body")
+const hbs       = require("koa-hbs")
 const logger    = require("koa-logger")
 const koaMount  = require("koa-mount")
 const passport  = require("koa-passport")
 const KoaRouter = require("koa-router")
 const session   = require("koa-session")
 const koaStatic = require("koa-static")
+const path      = require("path")
+
+const r = require("rethinkdbdash")()
 
 let router = new KoaRouter()
 
@@ -17,12 +21,29 @@ app.use(logger())
 app.keys = [ "1234567890" ]
 app.use(session(app))
 
-app.use(function *() {
-  var n = this.session.views || 0
+app.use(hbs.middleware({
+  viewPath: path.join(__dirname, "/views")
+}))
 
-  this.session.views = ++n
-  this.body = n + " views"
+// app.use(function *() {
+//   var n = this.session.views || 0
+
+//   this.session.views = ++n
+//   this.body = n + " views"
+// })
+
+router.get("/", function *() {
+  yield this.render("index", { title: "Quoth", body: "Hi!" })
 })
+
+router.get("/data", function *() {
+  let results = yield r.db("quoth").table("users").run()
+
+  this.type = "application/json"
+  this.body = JSON.stringify(results)
+})
+
+app.use(router.middleware())
 
 app.listen(3000, function () {
   console.log("Listening on port 3000.")
